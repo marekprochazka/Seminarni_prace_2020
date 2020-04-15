f76e20056fa3f9878450a8b03461be691abe530e
Marek Procházka <m.prochazka2002@gmail.com>
2020-04-15 10:34:06 +0200

    fixed pie error, save img moved to another branch

--- app.py
+++ app.py
@@ -232,62 +232,13 @@ class MarkoGebra(Tk):
 
         self._frame = None
         self.show_Setup_Frame(cont=Mathematical)
-        self.saver()
+        #self.saver()
 
 
     def on_exit(self):
         self.show_Setup_Frame()
-
-
         self.destroy()
-    def saver(self):
-        top = Toplevel()
-        top.wm_geometry("400x400")
-        top.wm_title("Uložit graf")
-        top.minsize(400,400)
-        top.maxsize(400,400)
 
-        name_label = t.Label(top,text="Název souboru:")
-        name_label.grid(row=0,column=0,padx=8)
-        name = t.Entry(top)
-        name.grid(row=0,column=1,sticky="we")
-        name_png = t.Label(top,text=".png")
-        name_png.grid(row=0,column=2,sticky="w")
-        direct_button = t.Button(top,text="Umístění",command=lambda:self.find_dir(direct,top,is_grid.state()))
-        direct_button.grid(row=1,column=0,columnspan=2,sticky="we")
-        direct = t.Label(top,text="")
-        direct.grid(row=1,column=2)
-        is_grid_label = t.Label(top, text="Neukládat s popisem os: ")
-        is_grid_label.grid(row=2,column=0)
-        is_grid = t.Checkbutton(top)
-        print(is_grid.state())
-        is_grid.grid(row=2,column=1)
-        send = t.Button(top,text="go",command=lambda:self.save_as_img(direct["text"],name.get(),top))
-        send.grid(row=3,column=0,columnspan=2,sticky="we")
-
-
-        top.columnconfigure(2,minsize=200)
-
-    def find_dir(self,dir_label,top,state):
-        if state == ("selected",):
-            a.axes.get_xaxis().set_visible(False)
-            a.axes.get_yaxis().set_visible(False)
-        file = filedialog.askdirectory()
-        if file:
-            dir_label["text"]=file
-        top.lift()
-
-    def save_as_img(self,file,name,top):
-        w, h = f.canvas.get_width_height()
-        buf = np.frombuffer(f.canvas.tostring_argb(), dtype=np.uint8)
-        buf.shape = (w, h, 4)
-        buf = np.roll(buf, 3, axis=2)
-        w, h, d = buf.shape
-        im = Image.frombytes("RGBA", (w, h), buf.tostring())
-        im.save(f"{file}/{name}.png")
-        top.destroy()
-        a.axes.get_xaxis().set_visible(True)
-        a.axes.get_yaxis().set_visible(True)
 
     def show_Setup_Frame(self, cont=None):
         global coordinates_all_list, coordinates_scatter, coordinates_plot, TO_ANIMATE, slices, cols, activities, explode, start_angle, bars, noises, dispersion, number, basic_gen
@@ -360,10 +311,11 @@ class MarkoGebra(Tk):
         elif TO_ANIMATE == 2:
             with(open("pie_save.txt", "r")) as save:
                 save_txt = save.readlines()
-                save_txt = [val.replace("\n","") for val in save_txt]
-                save_txt = [val.split(",") for val in save_txt]
+                if save_txt != ['\n', '\n', '\n']:
+                    save_txt = [val.replace("\n","") for val in save_txt]
+                    save_txt = [val.split(",") for val in save_txt]
 
-                if save_txt != []:
+
                     for index,_ in enumerate(save_txt[0]):
                         self.add_pie_data(data=[save_txt[0][index],save_txt[2][index],save_txt[1][index]],expl=int(save_txt[3][index]))
 
