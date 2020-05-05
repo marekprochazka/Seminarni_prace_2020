b20344cb855e3e3dfdbe5f5be07cdf0539475e6d
Marek Procházka <m.prochazka2002@gmail.com>
2020-05-05 16:14:11 +0200

    finished site skelet

--- web/MGWeb/App/urls.py
+++ web/MGWeb/App/urls.py
@@ -6,5 +6,14 @@ app_name = 'App'
 urlpatterns = [
 
     path("",home,name='home'),
-    path("env",env,name='env')
+    path("Prostredi",env,name='env'),
+    path("Metody/Matematicka",gmMat,name="gmMat"),
+    path("Metody/Kolac",gmPie,name="gmPie"),
+    path("Metody/Sloupcovy",gmBar,name="gmBar"),
+    path("Metody/Nahodny-sum",gmNo,name="gmNo"),
+    path("Konzole",cons,name="cons"),
+    path("Stazeni",down,name="down"),
+    path("Diskuze",disc,name="disk"),
+    path("O-autorovi",boutMe,name="boutMe")
+
 ]
--- web/MGWeb/App/views.py
+++ web/MGWeb/App/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 def home(request):
-    return render(request,"home.html")
+    return render(request, "home.html")
+
 
 def env(request):
-    return render(request,"env.html")
+    return render(request, "env.html")
+
+
+def gmMat(request):
+    return render(request, "gm-mat.html")
+
+
+def gmPie(request):
+    return render(request, "gm-Pie.html")
+
+
+def gmBar(request):
+    return render(request, "gm-bar.html")
+
+
+def gmNo(request):
+    return render(request, "gm-no.html")
+
+
+def cons(request):
+    return render(request, "cons.html")
+
+def down(request):
+    return render(request,"down.html")
+
+def disc(request):
+    return render(request,"disc.html")
+
+def boutMe(request):
+    return render(request,"boutme.html")
